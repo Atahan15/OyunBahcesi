2a470fac420d601fc0b82e1105cee02843f83dbe
Atahan15 <2021212052@cumhuriyet.edu.tr>
2025-05-19 11:05:29 +0300

    deprecated

--- Assets/Scripts/PuzzleScript/PuzzleLevelsScriptable.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PuzzleLevels", menuName = "ScriptableObjects/PuzzleLevel")]
-public class PuzzleLevelsScriptable : ScriptableObject
-{
-    public Level[] PuzzleLevels;
-    public int currentPuzzleLevel;
-}
